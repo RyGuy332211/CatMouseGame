e8423d733f9d72d18ef78c50c2afc47b61e6c33e
RyGuy332211 <143135450+RyGuy332211@users.noreply.github.com>
2025-12-06 22:49:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -255,6 +255,6 @@ setInterval(() => {
   }
 }, 50);
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(3030, () => {
+  console.log('Server running on port 3030');
 });
